504f4b2529de702d26f7315cd6f83a1aee73e04c
sudh4800@gmail.com <sudh4800@gmail.com>
2025-01-31 10:35:04 +0530

    added the Loader and Error handling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Main from "./Main";
 import Search from './Search';
@@ -7,15 +7,43 @@ import Box from './Box'
 import Summary from "./Summary";
 import WatchedList from "./WatchedList";
 import MovieList from "./MovieList";
+import Loader from "./Loader";
 import tempMovieData from "./Data/tempMovieData";
 import tempWatchedData from "./Data/tempWatchedData";
+import ErrorMessage from "./ErrorMessage";
+
+const KEY = '66bed1a5'
+const query = 'interstellar'
+const omdbApiEndpoint = `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
 
 export default function App() {
 
-  const [movies, setMovies] = useState(tempMovieData);
+  const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(tempWatchedData);
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState("")
 
 
+
+  useEffect(function () {
+
+    async function fetchMovies() {
+      try {
+        setIsLoading(true)
+        const res = await fetch(omdbApiEndpoint)
+        if (!res.ok) throw new Error('Failed to fetch')
+        const data = await res.json()
+        if (data.Response === 'False') throw new Error(data.Error)
+        setMovies(data.Search)
+      } catch (error) {
+        setError(error.message)
+      } finally {
+        setIsLoading(false)
+      }
+
+    }
+    fetchMovies()
+  }, [])
 
   return (
     <>
@@ -25,7 +53,9 @@ export default function App() {
       </Navbar>
       <Main>
         <Box>
-          <MovieList movies={movies} />
+          {isLoading && <Loader />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
         </Box>
         <Box>
           <Summary watched={watched} />
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating maxRating="5" messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+    <StarRating maxRating={10} color="red" size={30} defaultRating={3} onSetRating={(e) => console.log(e)} /> */}
   </React.StrictMode>
 );
 
